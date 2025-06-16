5d6fc761e610af83a312f5990a49c2f37b3c738d
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2025-06-16 18:50:14 +0530

    Update next.config.ts to include remote image patterns and clean up page.tsx structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+import AboutSection from "@/components/AboutSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
+import ProjectsSection from "@/components/Projects";
+import SkillsSection from "@/components/SkillSection";
+
 export default function Home() {
   return (
     <main className="min-h-screen">
-      {/* <Navbar />
+      <Navbar />
       <HeroSection />
       <AboutSection />
       <SkillsSection />
       <ProjectsSection />
       <ContactSection />
-      <Footer /> */}
+      <Footer />
     </main>
   );
 }
